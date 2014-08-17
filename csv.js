61282b5462be264b3cc70b99b053fd6ff741c258
K Nouroozi <knouroozi@gmail.com>
2014-08-17 13:21:47 -0400

    Fixed typo. Thanks @TuningGuide!

--- csv.js
+++ csv.js
@@ -220,9 +220,7 @@
 
   // Define this module
   if (typeof define === "function" && define.amd) {
-    define(function() {
-      return CSV;
-    });
+    define(function() { return CSV; });
   } else if (typeof module === "object" && module.exports) {
     module.exports = CSV;
   } else if (window && this === window) {
